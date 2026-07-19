90429f704e1ff31297859cfe0e5b55fc048a3804
andy <andyomar2312@gmail.com>
2026-07-19 02:22:45 -0500

    se agrega la estructura Producto

---
+++ productos.h
@@ -0,0 +1,13 @@
+#ifndef PRODUCTOS_H
+#define PRODUCTOS_H
+
+#include <QString>
+struct Producto{
+    QString codigo;
+    QString nombre;
+    QString tipo;
+    double precio;
+    int anio;
+};
+
+#endif // PRODUCTOS_H
